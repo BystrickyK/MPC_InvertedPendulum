50a95d41c66b79919d632d9aca15b14eaf41d734
000 <krystof.bystricky@fs.cvut.cz>
2020-02-22 03:26:32 +0100

    model + animation

---
+++ animation.m
@@ -0,0 +1,40 @@
+close all
+
+data = sol.y;
+Time = sol.x;
+
+rows_ = size(Time);
+rows_ = rows_(2);
+
+%
+f1 = figure;
+% animaci lze zrychlit pomocí úpravy kroku, nap?. p?i 1:3:rows_ se
+% vykresluje pouze každý t?etí vzorek
+for row = 1:1:rows_    
+    alpha = data(1, row)
+    Dalpha = data(2, row)
+    xc = data(3, row)
+    Dxc = data(4, row)
+   
+    %poloha kyvadla
+    [xp, yp] = pol2cart(alpha-pi/2, 5)
+    %% vykreslovani
+    cla
+    hold on
+    grid on
+    
+    quiver( xc, 0,...
+        xp, yp,...
+        'Color', 'Black',...
+        'LineWidth', 2,...
+        'Marker', '*')
+
+    time = Time(row);
+    title("T = " + time);
+    
+    xlim([-20, 20])
+    ylim([-15, 15])
+    axis equal
+
+   drawnow
+end
