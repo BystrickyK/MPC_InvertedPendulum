5f53951335d48eaa921519df7f37431bb21a604f
000 <krystof.bystricky@fs.cvut.cz>
2020-03-01 02:17:59 +0100

    +estimator

--- pendulumCart.m
+++ pendulumCart.m
@@ -1,4 +1,4 @@
-function dy = pendulumCart(Y,u,p)
+function dy = pendulumCart(Y,u,d,p)
     
     B_eq = p.B_eq;
     B_p = p.B_p;
@@ -21,7 +21,7 @@ function dy = pendulumCart(Y,u,p)
         dy = [        
         Y(3);
         Y(4);
-        (1.0./r_mp.^2.*(-B_eq.*J_p.*R_m.*r_mp.^2.*Y(3)-J_p.*K_g.^2.*eta_g.*k_m.*k_t.*Y(3)-B_eq.*M_p.*R_m.*l_p.^2.*r_mp.^2.*Y(3)+M_p.^2.*R_m.*l_p.^3.*r_mp.^2.*Y(4).^2.*s2+J_p.*K_g.*eta_g.*eta_m.*k_t.*r_mp.*u+M_p.^2.*R_m.*g.*l_p.^2.*r_mp.^2.*c2.*s2+J_p.*M_p.*R_m.*l_p.*r_mp.^2.*Y(4).^2.*s2+B_p.*M_p.*R_m.*l_p.*r_mp.^2.*Y(4).*c2-K_g.^2.*M_p.*eta_g.*k_m.*k_t.*l_p.^2.*Y(3)+K_g.*M_p.*eta_g.*eta_m.*k_t.*l_p.^2.*r_mp.*u))./(R_m.*(M_p.^2.*l_p.^2+J_eq.*J_p+J_p.*M_p-M_p.^2.*l_p.^2.*c2.^2+J_eq.*M_p.*l_p.^2));
-        -(1.0./r_mp.^2.*(B_p.*J_eq.*R_m.*r_mp.^2.*Y(4)+B_p.*M_p.*R_m.*r_mp.^2.*Y(4)+M_p.^2.*R_m.*g.*l_p.*r_mp.^2.*s2+M_p.^2.*R_m.*l_p.^2.*r_mp.^2.*Y(4).^2.*c2.*s2-B_eq.*M_p.*R_m.*l_p.*r_mp.^2.*Y(3).*c2+J_eq.*M_p.*R_m.*g.*l_p.*r_mp.^2.*s2-K_g.^2.*M_p.*eta_g.*k_m.*k_t.*l_p.*Y(3).*c2+K_g.*M_p.*eta_g.*eta_m.*k_t.*l_p.*r_mp.*u.*c2))./(R_m.*(M_p.^2.*l_p.^2+J_eq.*J_p+J_p.*M_p-M_p.^2.*l_p.^2.*c2.^2+J_eq.*M_p.*l_p.^2));
+        d(1)+(1.0./r_mp.^2.*(-B_eq.*J_p.*R_m.*r_mp.^2.*Y(3)-J_p.*K_g.^2.*eta_g.*k_m.*k_t.*Y(3)-B_eq.*M_p.*R_m.*l_p.^2.*r_mp.^2.*Y(3)+M_p.^2.*R_m.*l_p.^3.*r_mp.^2.*Y(4).^2.*s2+J_p.*K_g.*eta_g.*eta_m.*k_t.*r_mp.*u+M_p.^2.*R_m.*g.*l_p.^2.*r_mp.^2.*c2.*s2+J_p.*M_p.*R_m.*l_p.*r_mp.^2.*Y(4).^2.*s2+B_p.*M_p.*R_m.*l_p.*r_mp.^2.*Y(4).*c2-K_g.^2.*M_p.*eta_g.*k_m.*k_t.*l_p.^2.*Y(3)+K_g.*M_p.*eta_g.*eta_m.*k_t.*l_p.^2.*r_mp.*u))./(R_m.*(M_p.^2.*l_p.^2+J_eq.*J_p+J_p.*M_p-M_p.^2.*l_p.^2.*c2.^2+J_eq.*M_p.*l_p.^2));
+        d(2)-(1.0./r_mp.^2.*(B_p.*J_eq.*R_m.*r_mp.^2.*Y(4)+B_p.*M_p.*R_m.*r_mp.^2.*Y(4)+M_p.^2.*R_m.*g.*l_p.*r_mp.^2.*s2+M_p.^2.*R_m.*l_p.^2.*r_mp.^2.*Y(4).^2.*c2.*s2-B_eq.*M_p.*R_m.*l_p.*r_mp.^2.*Y(3).*c2+J_eq.*M_p.*R_m.*g.*l_p.*r_mp.^2.*s2-K_g.^2.*M_p.*eta_g.*k_m.*k_t.*l_p.*Y(3).*c2+K_g.*M_p.*eta_g.*eta_m.*k_t.*l_p.*r_mp.*u.*c2))./(R_m.*(M_p.^2.*l_p.^2+J_eq.*J_p+J_p.*M_p-M_p.^2.*l_p.^2.*c2.^2+J_eq.*M_p.*l_p.^2));
         ];
 end
--- plotRefresh.m
+++ plotRefresh.m
@@ -1,20 +1,19 @@
-function [] = plotRefresh(Ts, Xs, Wx, U, k, Ka)
-    figure(1);
-
-
-    
+function [] = plotRefresh(Ts, Xs, Xest, Wx, U, D, k, Ka)
+  
+figure(1);
     if(k-Ka == 0)
         Ka = Ka - 1;
     end
     
-    xlims = [Ts(k)-30, Ts(k)];
+    xlims = [Ts(k)-20, Ts(k)];
     time = Ts(k-Ka:k);
     
     subplot(221);
     hold on
     grid on
-    plot(time, Xs(k-Ka:k, 1), "LineWidth", 1.2, 'Color', 'r');
-    plot(time, Wx(k-Ka:k), "LineWidth", 1.2, 'LineStyle', '-.', 'Color', 'g');
+    plot(time, Xs(k-Ka:k, 1), "LineWidth", 2, 'Color', 'b');
+    plot(time, Wx(k-Ka:k), "LineWidth", 2, 'LineStyle', '-.', 'Color', 'g');
+    plot(time, Xest(k-Ka:k, 1), "LineWidth", 2, 'LineStyle', ':', 'Color', 'r');
 %     xlabel('Time t [$s$]','interpreter','latex')
 %     ylabel('$x_c$ [$m$]', 'Interpreter', 'Latex')
 %     title('$x_c$', 'Interpreter', 'Latex')
@@ -23,16 +22,8 @@ function [] = plotRefresh(Ts, Xs, Wx, U, k, Ka)
     subplot(222);
     hold on
     grid on
-    stairs(time, U(k-Ka:k), "LineWidth", 1.2, 'Color', 'k');
-%     xlabel('Time t [$s$]','interpreter','latex')
-%     ylabel('$\dot{x_c}$ [$\frac{m}{s}$]', 'Interpreter', 'Latex')
-%     title('$\dot{x_c}$', 'Interpreter', 'Latex') 
-     xlim(xlims);
-    
-    subplot(234);
-    hold on
-    grid on
-    plot(time, Xs(k-Ka:k, 2), "LineWidth", 1.2, 'Color', 'r');
+    plot(time, Xs(k-Ka:k, 2), "LineWidth", 2, 'Color', 'b');
+    plot(time, Xest(k-Ka:k, 2), "LineWidth", 2, 'LineStyle', ':', 'Color', 'r');
 %     xlabel('Time t [$s$]','interpreter','latex')
      xlim(xlims);
 
@@ -40,24 +31,48 @@ function [] = plotRefresh(Ts, Xs, Wx, U, k, Ka)
 %     title('$\alpha$', 'Interpreter', 'Latex')
      xlim(xlims);
     
-    subplot(235);
+    subplot(223);
     hold on
     grid on
-    plot(time, Xs(k-Ka:k, 3), "LineWidth", 1.2, 'Color', 'b');
+    plot(time, Xs(k-Ka:k, 3), "LineWidth", 2, 'Color', 'b');
+    plot(time, Xest(k-Ka:k, 3), "LineWidth", 2, 'LineStyle', ':', 'Color', 'r');
 %     xlabel('Time t [$s$]','interpreter','latex')
 %     ylabel('$\dot{x_c}$ [$\frac{m}{s}$]', 'Interpreter', 'Latex')
 %     title('$\dot{x_c}$', 'Interpreter', 'Latex')
      xlim(xlims);
 
-    subplot(236);
+    subplot(224);
     hold on
     grid on
-    plot(time, Xs(k-Ka:k, 4), "LineWidth", 1.2, 'Color', 'b');
+    plot(time, Xs(k-Ka:k, 4), "LineWidth", 2, 'Color', 'b');
+    plot(time, Xest(k-Ka:k, 4), "LineWidth", 2, 'LineStyle', ':', 'Color', 'r');
 %     xlabel('Time t [$s$]','interpreter','latex')
 %     ylabel('$\dot{\alpha}$ [$\frac{rad}{s}$]', 'Interpreter', 'Latex')
 %     title('$\dot{\alpha}$', 'Interpreter', 'Latex')
      xlim(xlims);    
     
-    drawnow
+% figure(3)
+%     subplot(311)
+%     hold on 
+%     grid on
+%     plot(time, D(k-Ka:k, 1), 'LineWidth', 1, 'Color', 'r');
+%     xlim(xlims);
+%     
+%     subplot(312)
+%     hold on 
+%     grid on
+%     plot(time, D(k-Ka:k, 2), 'LineWidth', 1, 'Color', 'r');
+%     xlim(xlims);
+    
+%     subplot(313);
+%     hold on
+%     grid on
+%     stairs(time, U(k-Ka:k), "LineWidth", 2, 'Color', 'r');
+%     xlabel('Time t [$s$]','interpreter','latex')
+%     ylabel('$\dot{x_c}$ [$\frac{m}{s}$]', 'Interpreter', 'Latex')
+%     title('$\dot{x_c}$', 'Interpreter', 'Latex') 
+%     xlim(xlims);
+    
+drawnow
 
     
