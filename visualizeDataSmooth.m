9c36057edf18db5380fa6c90a498a9667ded7381
000 <krystof.bystricky@fs.cvut.cz>
2020-03-03 23:22:34 +0100

    Zmena vystupni fce MPC, ladeni, fce pro hladsi vykreslovani animace

---
+++ visualizeDataSmooth.m
@@ -0,0 +1,40 @@
+clc
+close all
+
+addpath('functions')
+addpath('gif')
+
+
+data = load('ResultsMPC8.mat');
+data = data.sol;
+Xc = data.Xc;
+Tc = data.Tc;
+computingTimes = data.computingTimes;
+
+samples = length(Tc);
+
+kRefreshPlot = 10; %vykresluje se pouze po kazdych 'kRefreshPlot" samplech
+kRefreshAnim = 1; % ^
+
+figure("Name", "Computing times")
+bar(Ts(1:end-1), computingTimes);
+
+% animRefresh(Ts,Xs,[],1);
+% gif('NLMPC_Swingup_dt10_sawtooth_lowV.gif')
+tic
+for k = 2:15:samples
+    %% Vizualizace
+    
+    if(mod(k,kRefreshAnim)==0)
+        animRefresh(Tc,Xc,[],k);
+        title(Tc(k))
+%         gif
+    end
+    
+    if(mod(k,10000)==0)
+        disp("Time for 10000 samples:" + toc)
+        tic
+    end
+    
+    
+end
